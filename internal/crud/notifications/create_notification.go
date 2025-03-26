6de7a5d258a6332f741d8916d463ff21cec5fb08
kirmada150905 <174258997+kirmada150905@users.noreply.github.com>
2025-03-27 00:36:43 +0530

    chore: updated notification model

--- internal/crud/notifications/create_notification.go
+++ internal/crud/notifications/create_notification.go
@@ -1,56 +1,11 @@
 package notification
 
-import (
-	"bitsCarPool_back/internal/models"
-	"context"
-	"errors"
-	"fmt"
+import "bitsCarPool_back/internal/models"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
-func (s *notificationService) CreateNotification(notification *models.Notification) (string, error) {
-	if notification == nil {
-		return "", errors.New("invalid data")
-	}
-
-
-	notification.NotificationID = primitive.NewObjectID().Hex()
-
-	collection := s.db.Database(s.database).Collection("users")
-
-	// Add to recipient's notifications_received
-	receivedUpdate := bson.M{
-		"$push": bson.M{
-			"notifications_received": notification,
-		},
-	}
-
-	_, err := collection.UpdateOne(
-		context.Background(),
-		bson.M{"_id": notification.To},
-		receivedUpdate,
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to update notifications_received for user %s: %v", notification.To, err)
-	}
-
-	// Add to sender's notifications_sent
-	sentUpdate := bson.M{
-		"$push": bson.M{
-			"notifications_sent": notification,
-		},
-	}
-
-	_, err = collection.UpdateOne(
-		context.Background(),
-		bson.M{"_id": notification.From},
-		sentUpdate,
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to update notifications_sent for user %s: %v", notification.From, err)
-	}
-
-	return notification.NotificationID, nil
+func (s *notificationService) CreateNotification(notification *models.Notification) (string, error){
+	// go to collection: notifications
+	//access from and put in sent
+	//access to and put in received
+	return "success", nil
 }
+	
--- internal/models/notification.go
+++ internal/models/notification.go
@@ -1,9 +1,20 @@
 package models
-
-type Notification struct {
-	NotificationID string`json:"notification_id" bson:"notification_id,omitempty"`
+type Notification struct{
+	User_id string `json:"user_id" bson:"user_id"`
+	Sent []SentNotification `json:"sent_notifications" bson:"sent_notifications"`
+	Received []SentNotification `json:"received_notifications" bson:"received_notifications"`
+}
+type SentNotification struct {
 	From    string `json:"from" bson:"from"`
-	To      string `json:"to" bson:"to"`
+	To string `json:"to" bson:"to"`
+	TripId  string `json:"trip_id" bson:"trip_id"`
+	Notes   string `json:"notes" bson:"notes"`
+	Status string  `json:"status" bson:"status"` // "accept, decline"
+}
+
+type ReceivedNotification struct {
+	From    string `json:"from" bson:"from"`
+	To string `json:"to" bson:"to"`
 	TripId  string `json:"trip_id" bson:"trip_id"`
 	Notes   string `json:"notes" bson:"notes"`
 	Status string  `json:"status" bson:"status"` // "accept, decline"
